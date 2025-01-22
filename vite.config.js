565fb9574627aa055f7f7bb0d89a0c3f53c27891
LLPFP <lopezpuigluis@fpllefia.com>
2025-01-22 23:27:20 +0100

    vite.config fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
- root: path.resolve(__dirname, 'src'),
- publicDir: path.resolve(__dirname, 'public'),
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
+  root: path.resolve(__dirname, "src"), // Mantienes 'src' como root si es necesario
+  publicDir: path.resolve(__dirname, "public"),
+  build: {
+    outDir: path.resolve(__dirname, "dist"), // Cambia esto a una ruta absoluta fuera de 'src'
+    emptyOutDir: true, // Limpia el directorio de salida antes de construir
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
+  },
 });
